7734dc97c1726081f40e79fbe092ddcf9be73080
Shosh Etzion <shosh.rosencweig@gmail.com>
2021-08-05 16:59:23 +0300

    make it better

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import http from 'http';
 import weatherRoutes from './routes/weather.js';
+import path from 'path';
 
 const app = express();
 
@@ -26,6 +27,6 @@ http.createServer(app).listen(port, ()=>{
 //app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+// });
